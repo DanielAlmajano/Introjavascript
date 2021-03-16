067413d7b35340b635225dd55a51ed0040974c7a
Daniel <daniel.almajano.hernandez@gmail.com>
2021-03-16 22:22:55 +0100

    variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+/**
+ * Variables: var, let y const
+ * Suma de vectores/array
+ */
+//arrays:
+var lista1 = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+let lista2 = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+let lista3 = [];
+
+for (let i = 0; i <= lista1.length; i++) {
+    lista3.push(lista1[i] + lista2[i]);
+}
+console.log(lista3)
+
+
+//
