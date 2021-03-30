2da634181b67d38733461945a32afce965af03b6
Daniel <daniel.almajano.hernandez@gmail.com>
2021-03-30 23:14:21 +0200

    basic functions

---
+++ src/funciones/funcion1.js
@@ -0,0 +1,18 @@
+//Rallada
+function double(n, f) {
+    return f(n);
+}
+fun = (n => 2 * n);
+console.log(double(10, fun));
+
+
+
+//Version facil
+function double1(n) {
+    return n * 2
+}
+console.log(double1(100));
+
+//Version arrow
+func1 = (n => 2 * n);
+console.log(func1(120));
---
+++ src/funciones/funcion2.js
@@ -0,0 +1,4 @@
+function suma(n, m) {
+    return m + n;
+}
+console.log(suma(1, 2));
